b14a117a65d1649ae0adde37bdae8249ba6ebcc7
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-09-01 15:13:14 +0700

    created homepage files

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavBar } from "../../components/Navigation/index";
+import { Button } from "../../components/common/Button";
+import StackedRateCard from "../../components/common/StackedRateCard";
+
+export const Homepage = () => {
+  return (
+    <div className="container mx-auto">
+      <NavBar />
+      <div className="flex justify-between my-2 w-full">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-7xl font-bold leading-tight">
+            Be The Fastest in Delivering Your{" "}
+            <span className="text-red-600">Food</span>
+          </h1>
+          <p className="text-lg font-medium my-4 w-[90%]">
+            Our job is to filling your tummy with delicious food and with fast
+            nad free delivery
+          </p>
+          <Button variant="primary">Sign Up</Button>
+          <StackedRateCard />
+        </div>
+        <img src="src/assets/Banner.png" className="w-[60%] h-auto " />
+      </div>
+    </div>
+  );
+};
