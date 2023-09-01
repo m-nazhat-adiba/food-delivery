3f13b2198e9d718ca695d0970c46448f5a297f46
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-09-02 05:43:50 +0700

    added outline variant

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,6 +2,7 @@ import React from "react";
 const VARIANTS = {
   primary: "border-0 bg-[#BE0B05] text-[#ffffff] hover:bg-red-600",
   secondary: "border-0 text-red-700 hover:bg-gray-50 hover:bg-opacity-20",
+  outline: "border-2 text-red-700 border-red-700 hover:bg-red-100",
   link: "border-0 text-black font-medium hover:text-red-700",
 };
 
