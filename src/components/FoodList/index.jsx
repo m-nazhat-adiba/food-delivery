5c0e7bd2772c4844bad8acb4619661a080b219b0
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-05 13:54:36 +0700

    corrected components source address

--- src/components/FoodList/index.jsx
+++ src/components/FoodList/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { ProductCard } from "./ProductCard";
+import { ProductCard } from "./container/ProductCard";
 
 export const ProductList = () => {
   const [foodData, setFoodData] = useState([]);
