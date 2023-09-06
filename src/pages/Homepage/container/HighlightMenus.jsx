afc3411c4613a25b2d577ddc0475f3d4c82a6169
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-06 15:21:14 +0700

    added state and switch function for filter tabs

--- src/pages/Homepage/container/HighlightMenus.jsx
+++ src/pages/Homepage/container/HighlightMenus.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ProductList } from "../../../components/FoodList";
+import { Icon } from "@iconify/react";
+import clsx from "clsx";
 
 const HighlightMenus = () => {
+  const [filter, setFilter] = useState("");
+  const handleFilter = (filter = "main-dish") => {
+    switch (filter) {
+      case "main-dish":
+        setFilter("main-dish");
+        break;
+      case "beverage":
+        setFilter("beverage");
+        break;
+      case "all":
+        setFilter("");
+      default:
+        null;
+    }
+  };
+  useEffect(() => {
+    handleFilter();
+  }, []);
+
   return (
     <div className="flex flex-col px-16">
       <div className="flex flex-col w-1/2 mx-auto mb-10">
@@ -16,7 +37,75 @@ const HighlightMenus = () => {
         <div>
           <img src="src/assets/promo-banner.png" />
         </div>
-        <ProductList />
+        <div className="flex gap-16">
+          <div className="flex flex-col w-80 gap-2">
+            <button
+              onClick={() => handleFilter("main-dish")}
+              className={clsx(
+                "group flex items-center text-xl px-4 py-4 rounded-full ",
+                filter === "main-dish"
+                  ? "bg-red-600 text-white"
+                  : "hover:text-red-600"
+              )}
+            >
+              <div
+                className={clsx(
+                  "group-hover:bg-white p-3 rounded-full mr-2",
+                  filter === "main-dish" ? "bg-white text-white" : null
+                )}
+              >
+                <Icon
+                  icon="akar-icons:utensils"
+                  className="text-black group-hover:text-red-600"
+                />
+              </div>
+              Main Dish
+            </button>
+            <button
+              onClick={() => handleFilter("beverage")}
+              className={clsx(
+                "group flex items-center text-xl px-4 py-4 rounded-full ",
+                filter === "beverage"
+                  ? "bg-red-600 text-white"
+                  : "hover:text-red-600"
+              )}
+            >
+              <div
+                className={clsx(
+                  "group-hover:bg-white p-3 rounded-full mr-2",
+                  filter === "beverage" ? "bg-white text-white" : null
+                )}
+              >
+                <Icon
+                  icon="akar-icons:wine-glass"
+                  className="text-black group-hover:text-red-600"
+                />
+              </div>
+              Beverage
+            </button>
+            <button
+              onClick={() => handleFilter("all")}
+              className={clsx(
+                "group flex items-center text-xl px-4 py-4 rounded-full",
+                filter === "" ? "bg-red-600 text-white" : "hover:text-red-600"
+              )}
+            >
+              <div
+                className={clsx(
+                  "group-hover:bg-white p-3 rounded-full mr-2",
+                  filter === "" ? "bg-white text-white" : null
+                )}
+              >
+                <Icon
+                  icon="akar-icons:dot-grid-fill"
+                  className="text-black group-hover:text-red-600"
+                />
+              </div>
+              Show All
+            </button>
+          </div>
+          <ProductList />
+        </div>
       </div>
     </div>
   );
