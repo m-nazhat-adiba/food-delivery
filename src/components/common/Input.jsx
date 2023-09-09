a6ec81d6f7eaec06dfe865e2611c0effc5d25e75
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-09 13:40:41 +0700

    added focus state

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
 
-const InputField = ({ id, placeholder, onChange }) => {
+const InputField = ({ id, placeholder, onChange, data }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      setIsFocused(true);
+    } else {
+      setIsFocused(false);
+    }
+  }, [data]);
+
   return (
     <div>
       <label className="flex flex-col relative focus group">
         <input
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => (data ? null : setIsFocused(false))}
           onChange={onChange}
           type={id}
           name={id}
           required
           className="border-2 border-gray-300 px-4 py-2 text-xs"
         />
-        <span className="absolute text-xs transform translate-y-3 leading-3 left-2 transition group-focus-within:-translate-y-4">
+        <span
+          className={clsx(
+            "absolute text-xs transform translate-y-3 leading-3 left-2 transition ",
+            isFocused ? "-translate-y-4" : null
+          )}
+        >
           {placeholder}
         </span>
         {/* <div className="ml-auto text-xs text-gray-500 leading-5">
