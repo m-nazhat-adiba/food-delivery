4546f6a3cb9a044ff06dac3628d5054c2e81b23c
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-09 15:16:06 +0700

    added POST API

--- src/pages/LoginRegister/container/Register.jsx
+++ src/pages/LoginRegister/container/Register.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../../../components/common/Input";
 import { Icon } from "@iconify/react";
+import axios from "axios";
 
 const Register = ({ handleSwitchToRegister }) => {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [pw2, setPw2] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePw = (e) => {
+    setPw(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      name: email,
+      username: email,
+      password: pw,
+      roleId: 2,
+    };
+    axios
+      .post("https://api.mudoapi.tech/register", payload)
+      .then((res) => {
+        console.log(res);
+        handleSwitchToRegister("close");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log("eventHandler:", email, pw, pw2);
   return (
     <div>
       <h1 className="text-xl mb-4">Create your Account</h1>
@@ -12,10 +43,18 @@ const Register = ({ handleSwitchToRegister }) => {
       </button>
       <p className="text-xs my-6">or sign up with email</p>
       <form className="flex flex-col gap-6">
-        <InputField id="email" placeholder="Email Address" />
-        <InputField id="password" placeholder="Password" />
-        <InputField id="password" placeholder="Password Confirmation" />
+        <InputField
+          onChange={handleChangeEmail}
+          id="username"
+          placeholder="Email Address"
+        />
+        <InputField
+          onChange={handleChangePw}
+          id="password"
+          placeholder="Password"
+        />
         <button
+          onClick={handleSubmit}
           type="submit"
           className="mt-4 px-4 py-2 bg-red-700 text-white rounded-full hover:bg-red-600"
         >
