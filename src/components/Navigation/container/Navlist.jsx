bad9b3f73ce3f839d5c0ca907c79b6561b9c445d
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-09 10:44:54 +0700

    added Link

--- src/components/Navigation/container/Navlist.jsx
+++ src/components/Navigation/container/Navlist.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "../../common/Button";
+import { Link } from "react-router-dom";
 
 const Navlist = () => {
   return (
     <div className="flex gap-2">
       <Button variant="link">Why Us?</Button>
       <Button variant="link">Services</Button>
-      <Button variant="link">Menu</Button>
+      <Link to={"/menus"}>
+        <Button variant="link">Menu</Button>
+      </Link>
       <Button variant="link">Contact</Button>
     </div>
   );
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navlist from "./container/Navlist";
 import NavUtility from "./container/NavUtility";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="flex justify-between py-5">
-      <img src="/src/assets/icon.png" className="h-12 w-auto" />
+      <Link to={"/"}>
+        <img src="/src/assets/icon.png" className="h-12 w-auto" />
+      </Link>
       <Navlist />
       <NavUtility />
     </div>
