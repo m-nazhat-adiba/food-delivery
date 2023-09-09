e6ffe87e407fcb2b6f2057e1a687d025a35d3243
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-09 10:39:03 +0700

    edited img src address

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -5,7 +5,7 @@ import NavUtility from "./container/NavUtility";
 const NavBar = () => {
   return (
     <div className="flex justify-between py-5">
-      <img src="src/assets/icon.png" className="h-12 w-auto" />
+      <img src="/src/assets/icon.png" className="h-12 w-auto" />
       <Navlist />
       <NavUtility />
     </div>
