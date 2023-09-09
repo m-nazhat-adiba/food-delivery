348f6c339ca7c37da379d9c8e2992c3e32ae509a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-09 10:41:49 +0700

    added handleDetail()

--- src/pages/Menus/index.jsx
+++ src/pages/Menus/index.jsx
@@ -3,8 +3,14 @@ import NavBar from "../../components/Navigation";
 import Header from "./container/Header";
 import ProductMenu from "./container/ProductMenu";
 import Footer from "../Homepage/container/Footer";
+import { useNavigate } from "react-router";
 
 function index() {
+  const navigate = useNavigate();
+  const handleDetail = (id) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div>
       <div className="container mx-auto">
@@ -14,7 +20,7 @@ function index() {
         <Header />
       </div>
       <div className="container mx-auto mt-10 px-12 flex flex-col items-center gap-10">
-        <ProductMenu />
+        <ProductMenu handleDetail={handleDetail} />
       </div>
       <div className="container mx-auto mt-24 mb-10">
         <Footer />
