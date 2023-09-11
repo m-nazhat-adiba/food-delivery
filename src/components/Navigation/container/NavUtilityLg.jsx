4947e0e391b6abdce4b0b3cc7cd58dccbb6518de
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-11 23:52:59 +0700

    first commit

---
+++ src/components/Navigation/container/NavUtilityLg.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
+import clsx from "clsx";
+
+const NavUtilityLg = () => {
+  const [searchView, setSearchView] = useState(false);
+
+  const handleSearchView = (isOpen = "close") => {
+    if (isOpen === "open") {
+      setSearchView(true);
+    } else {
+      setSearchView(false);
+    }
+  };
+  return (
+    <div className="flex items-center gap-2 relative lg:hidden">
+      <div
+        className={clsx(
+          "flex border-2 w-fit items-center rounded-full absolute right-7 bg-white",
+          {
+            "border-gray-300": searchView,
+            "border-transparent": !searchView,
+          }
+        )}
+      >
+        {searchView ? (
+          <Icon
+            onClick={() => handleSearchView("close")}
+            icon="akar-icons:x-small"
+            className="text-2xl mx-2"
+          />
+        ) : null}
+        <input
+          className={clsx(
+            "my-2  bg-transparent focus:outline-none duration-300",
+            {
+              "md:w-[30vw] w-[40vw]": searchView,
+              "w-0": !searchView,
+            }
+          )}
+          placeholder="Search Menu"
+        />
+        <Icon
+          onClick={() => handleSearchView(searchView ? "close" : "open")}
+          icon="akar-icons:search"
+          className="text-2xl mx-2"
+        />
+      </div>
+      <Icon icon="akar-icons:shopping-bag" className="text-2xl" />
+    </div>
+  );
+};
+
+export default NavUtilityLg;
