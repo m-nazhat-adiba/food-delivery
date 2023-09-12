156d8ce19ff8e5e936429927a5f26e510b12f274
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-12 20:29:46 +0700

    added props for handling detail function and menu, pagination state

--- src/pages/Menus/container/ProductMenu.jsx
+++ src/pages/Menus/container/ProductMenu.jsx
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { ProductCard } from "../../../components/FoodList/container/ProductCard";
 import DefaultPagination from "../../../components/common/Pagination";
 
-function ProductMenu({ handleDetail }) {
-  const [menus, setMenus] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nextPage, setNextPage] = useState(1);
-
-  const getMenus = () => {
-    axios
-      .get(
-        `https://api.mudoapi.tech/menus?perPage=10&page=${currentPage}&name=`
-      )
-      .then((res) => {
-        const data = res?.data?.data?.Data;
-        setCurrentPage(res.data.data.currentPage);
-        setNextPage(res.data.data.nextPage);
-        setMenus(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    getMenus();
-  }, [currentPage]);
-
+function ProductMenu({
+  handleDetail,
+  menus,
+  currentPage,
+  nextPage,
+  setCurrentPage,
+}) {
   return (
     <div className="w-full flex flex-col items-center gap-12">
       <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-8">
