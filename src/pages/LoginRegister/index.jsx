aa977e119fa6824be12ee96072fa5e8fcdb4b8ba
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-12 11:38:54 +0700

    edited src

--- src/pages/LoginRegister/index.jsx
+++ src/pages/LoginRegister/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Login from "./container/Login";
 import Register from "./container/Register";
+import Logo from "../../assets/icon.png";
+import Banner from "../../assets/login-banner.png";
 
 const LoginPage = () => {
   const [showRegister, setShowRegister] = useState(false);
@@ -19,10 +21,10 @@ const LoginPage = () => {
     >
       <div className="bg-white flex flex-col md:flex-row shadow-xl rounded-lg w-[90%] xl:w-[75%] 2xl:w-[60%] mx-auto">
         <div className="block w-full md:hidden">
-          <img src="/src/assets/icon.png" className="w-full px-6 pt-8" />
+          <img src={Logo} className="w-full px-6 pt-8" />
         </div>
         <div className="hidden md:block w-[60%] h-auto p-8">
-          <img src="/src/assets/login-banner.png" className="rounded-xl" />
+          <img src={Banner} className="rounded-xl" />
         </div>
         <div className="text-center mx-8 lg:mx-auto my-auto">
           {showRegister ? (
