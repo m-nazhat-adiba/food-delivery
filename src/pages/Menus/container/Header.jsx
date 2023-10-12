12747664456527be85a9bc0424fa7ff2956abea6
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-12 09:46:12 +0700

    edited src

--- src/pages/Menus/container/Header.jsx
+++ src/pages/Menus/container/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <img className="my-4 md:my-10" src="./src/assets/promo-banner.png" />
+      <img className="my-4 md:my-10" src="./assets/promo-banner.png" />
       <h1 className="text-2xl md:text-4xl xl:text-6xl font-medium mt-6 md:mt-16 xl:mt-20">
         Our Menu
       </h1>
